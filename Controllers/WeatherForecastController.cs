067c8875febfef959a33c7c897f0b3f360ec852c
deino2 <trab.filipe@hotmail.com>
2021-07-19 15:58:29 +0100

    Teste1

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace NetProject.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "CENAS"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "CENAS2"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
